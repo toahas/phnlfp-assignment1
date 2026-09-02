e211ee1e0eebb0c90eb649f2c2c959bfc8331d1e
toahas <toahasiddique.ts@gmail.com>
2026-09-02 08:49:23 -0500

    add question3 answer

--- answer.js
+++ answer.js
@@ -7,6 +7,7 @@ describeValue = (value)=>{
 }
 
 //question2
+
 getDayType = (day)=>{
     day=day.toLowerCase();
     switch(day){
@@ -21,5 +22,22 @@ getDayType = (day)=>{
             return "Working Day";
         default:
             return "Invalid Day";
+    }
+}
+
+//question3
+
+validateUsername = (username)=>{
+    if(username.length<4){
+        return "Too Short";
+    }
+    else if(username.includes(" ")){
+        return "No Space Allowed";
+    }
+    else if(username.toLowerCase().includes("admin")){
+        return "Reserved Word";
+    }
+    else {
+        return "Available";
     }
 }

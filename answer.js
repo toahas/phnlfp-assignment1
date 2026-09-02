8d8dccaf02af11a12a81025fd905e9382ff2eab1
toahas <toahasiddique.ts@gmail.com>
2026-09-02 08:19:59 -0500

    add question2 answer

--- answer.js
+++ answer.js
@@ -5,3 +5,21 @@ describeValue = (value)=>{
     let val2 = value? "truthy" : "falsy";
     return `${val1} | ${val2}`;
 }
+
+//question2
+getDayType = (day)=>{
+    day=day.toLowerCase();
+    switch(day){
+        case "friday":
+        case "saturday":
+            return "Weekend";
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+        default:
+            return "Invalid Day";
+    }
+}
